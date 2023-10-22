50800fb375c2987ebcc66c860c1f8082fcf8ce04
saezeric <128070029+saezeric@users.noreply.github.com>
2023-10-22 20:01:58 +0200

    Update main.js

--- ejercicio5/main.js
+++ ejercicio5/main.js
@@ -100,14 +100,20 @@ function moverFicha2(){
 
     // seleccionamos casilla actual
     selector= `#casillas2 #c${numCasillaF2}`
+    //quitamos la ficha de donde esta actualmente
     document.querySelector(selector).innerHTML=''
+    // llamamos a la funcion
     tirarDado2()
+    // si casilla es menor que 20 que se pinte la ficha
     if(numCasillaF2<20){
+        // pintamos la ficha
         selector= `#casillas2 #c${numCasillaF2}`
         document.querySelector(selector).innerHTML = ficha2
     }
 
+    // si la casillaf1 llega a la de su rival, su rival vuelve al inicio
     if(numCasillaF2==numCasillaF1){
+        //hacemos volver a la ficha rival al inicio
         selector= `#casillas1 #c${numCasillaF1}`
         document.querySelector(selector).innerHTML=''
         numCasillaF1 = 1
@@ -115,17 +121,22 @@ function moverFicha2(){
         document.querySelector(selector).innerHTML = ficha1
     }
 
+    // si nos pasamos de la casilla 20 nos hace volver
     if(numCasillaF2>20){
+        // pintamos la casilla
         diferencia = numCasillaF2 - 20
         numCasillaF2 = 20 - diferencia
         selector= `#casillas2 #c${numCasillaF2}`
         document.querySelector(selector).innerHTML = ficha2
     }
 
+    // si la casilla cae en 20 directamente has ganado
     if(numCasillaF2==20){
+        // Quitamos las fichas de donde estaban en el tablero
         document.querySelector(reset1).innerHTML=''
         document.querySelector(reset2).innerHTML=''
         if(confirm("Felicidades Jugador 2, Has ganado!!! Quieres reiniciar partida?") == true){
+            // reseteamos las casillas al inicio en caso de darle que si
             numCasillaF1 = 1
             numCasillaF2 = 1
             reset1= `#casillas1 #c${numCasillaF1}`
