9ba9f8880081d92537a2952a9000d0982244e847
saezeric <128070029+saezeric@users.noreply.github.com>
2023-10-22 22:56:45 +0200

    Update main.js

--- historia1/main.js
+++ historia1/main.js
@@ -1,6 +1,8 @@
 console.log("Hola a todos!!")
 
+// Creo la funcion para Pintar el Ranking
 function pintaRanking(){
+// Creamos la funcion que pinta el ranking en HTML
 let ranking= `
 <h2 class="text-center text-light">Ranking</h2>
     <table class="table table-dark align-middle">
@@ -35,10 +37,11 @@ let ranking= `
         <tfoot></tfoot>
     </table>
 `
-
+// Pintamos el ranking
 document.querySelector("#ranking").innerHTML = ranking
 }
 
+// Creamos las partidas
 const partidas = [{
     avatar: "https://www.svgrepo.com/show/384679/account-avatar-profile-user-3.svg",
     nick: "Eric",
@@ -58,7 +61,9 @@ const partidas = [{
     fecha: "10 Agosto 2023"
 }]
 
+// Creamos la funcion que pinta Tabla
 function pintaTabla(partidas){
+    // Creamos la variable que pinta la tabla
     let tabla=`
     <h2 class="text-center text-light">Partidas</h2>
         <div class="input-group mb-3">
@@ -87,7 +92,7 @@ function pintaTabla(partidas){
                 </tr>
             </theader>
             <tbody>`
-                    
+        // Iteramos para imprimir las partidas anteriores            
         partidas.forEach(element => {
             tabla+=`
             <tr>
@@ -98,16 +103,18 @@ function pintaTabla(partidas){
             </tr>
             `
         });
-                    
+            //Cerramos la tabla una vez finalizada la iteración       
             tabla+= `
             </tbody>
             <tfoot></tfoot>
         </table>
     `
+// Pintamos la tabla
 document.querySelector("#partidas").innerHTML = tabla
 console.log(tabla)
 }
 
+// Creamos una nueva partida diferente al resto
 const datosEjemploPartida = {
     avatar:"https://www.svgrepo.com/show/384672/account-avatar-profile-user-7.svg",
     nick: "MANUEL",
@@ -115,19 +122,25 @@ const datosEjemploPartida = {
     fecha:"21 MAYO 2023"
 }
 
+// Creamos una funcion que inserta nuevas partidas
 function insertaNuevaPartida(partidas){
     console.log("Guardando partida")
+    // Empujamos la partida nueva creada anteriormente
     partidas.push(datosEjemploPartida)
     console.log(partidas)
 }
 
+// Creamos una funcion que pinta los datos de la partida
 function pintaDatosPartida(partidas){
+    // Creamos una alerta para confirmar el guardado de partida
     if (confirm("Quieres GUARDAR PARTIDA?") == true) {
-        
+        // insertamos la nueva partida
         insertaNuevaPartida(partidas)
+        // pintamos la tabla
         pintaTabla(partidas)
-
+    // Si le decimos que no, se pintaran las partidas anteriores menos la nueva
       } else {
+        // pintamos la nueva tabla
         pintaTabla(partidas)
         console.log("No se ha guardado ninguna partida :D");
       }
